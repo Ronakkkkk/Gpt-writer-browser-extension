f632764596eb2f4c8d9869ee72aa594bf45653f4
Ronak <ronak01.raj@gmail.com>
2023-08-06 20:31:00 +0530

    extension ui

---
+++ index.js
@@ -0,0 +1,8 @@
+const saveKey = () => {}
+
+const changeKey = () => {}
+
+document.getElementById('save_key_button').addEventListener('click', saveKey);
+document
+  .getElementById('change_key_button')
+  .addEventListener('click', changeKey);
